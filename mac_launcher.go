537e5c12708fe3cc53aa7f338eb8f87bc67ea8ef
Andy Kreling <krelinga@gmail.com>
2024-01-11 23:58:29 +0000

    plumb up docker's stdout, stdin, and stderr to the launcher's terminal.

--- mac_launcher.go
+++ mac_launcher.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "log"
+    "os"
     "os/exec"
 )
 
@@ -10,6 +11,9 @@ func main() {
     fmt.Println("Hello from launcher!")
 
     docker_cmd := exec.Command("docker", "run", "--pull", "always", "krelinga/video-tool-box")
+    docker_cmd.Stdout = os.Stdout
+    docker_cmd.Stderr = os.Stderr
+    docker_cmd.Stdin = os.Stdin
     if err := docker_cmd.Run(); err != nil {
         log.Fatal(err)
     }
