55e3cd533913f4ff859222ab39ce7266e3ee454f
Andy Kreling <krelinga@gmail.com>
2024-01-11 23:48:36 +0000

    mac launcher starts a docker container, which runs main().  Input and output plumbing not working yet.

--- mac_launcher.go
+++ mac_launcher.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os/exec"
+)
 
 func main() {
     fmt.Println("Hello from launcher!")
+
+    docker_cmd := exec.Command("docker", "run", "--pull", "always", "krelinga/video-tool-box")
+    if err := docker_cmd.Run(); err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Docker run successful.")
 }
