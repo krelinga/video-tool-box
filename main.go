1e02d18b178b6b3796e20406be7bc4e730550582
Andy Kreling <krelinga@gmail.com>
2024-01-12 21:46:30 +0000

    update main() to list the contents of the root directory.

--- main.go
+++ main.go
@@ -8,12 +8,25 @@ import (
     cli "github.com/urfave/cli/v2"
 )
 
+func listDir(dir string) {
+    fmt.Println("listing dir", dir)
+    entries, err := os.ReadDir(dir)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, entry := range entries {
+        fmt.Println(entry.Name())
+    }
+}
+
 func main() {
     fmt.Println("Hello from main!")
     app := &cli.App{
         Name: "vtb",
         Action: func(*cli.Context) error {
             fmt.Println("main action.")
+            listDir("/")
             return nil
         },
     }
