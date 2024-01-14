c63f58b597355cc51d0c8f8e3ea59dc02dc333dc
Andy Kreling <krelinga@gmail.com>
2024-01-14 02:49:21 +0000

    rip out tmm_movies and tmm_shows directories, renames across mounts did not work.

--- main.go
+++ main.go
@@ -34,29 +34,11 @@ func main() {
                 Value: "/",
                 Usage: "Directory to do work in.",
             },
-            &cli.StringFlag{
-                Name: "tmm_movies",
-                Value: "",
-                Usage: "Tiny Media Manager movies dir.",
-            },
-            &cli.StringFlag{
-                Name: "tmm_shows",
-                Value: "",
-                Usage: "Tiny Media Manager shows dir.",
-            },
         },
         Action: func(c *cli.Context) error {
             fmt.Println("main action.")
             work_dir := c.String("work_dir")
             listDir(work_dir)
-            tmm_movies := c.String("tmm_movies")
-            if len(tmm_movies) > 0 {
-                listDir(tmm_movies)
-            }
-            tmm_shows := c.String("tmm_shows")
-            if len(tmm_shows) > 0 {
-                listDir(tmm_shows)
-            }
 
             return nil
         },
