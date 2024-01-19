5e2be0ab845d065d8de601fe51656192025cbbd4
Andy Kreling <krelinga@gmail.com>
2024-01-19 04:46:26 +0000

    first attempt to run handbrake from vtb.

--- trans.go
+++ trans.go
@@ -2,9 +2,24 @@ package main
 
 import (
     "errors"
+    "fmt"
+    "os"
+    "os/exec"
 
     cli "github.com/urfave/cli/v2"
 )
+
+type handbrakeFlags []string
+
+var gHandbrakeProfile = map[string]handbrakeFlags{
+    "mkv_h265_1080p30": {
+        "-Z", "Matroska/H.265 MKV 1080p30",
+        "--all-audio",
+        "--non-anamorphic",
+        "--all-subtitles",
+        "--subtitle-burned=none",
+    },
+}
 
 func cmdTrans() *cli.Command {
     fn := func(c *cli.Context) error {
@@ -12,6 +27,32 @@ func cmdTrans() *cli.Command {
         if len(handbrake) == 0 {
             return errors.New("'trans' command only available when --handbrake is set")
         }
+
+        input, err := getEnvVar("VTB_INPUT")
+        if err != nil { return err }
+        output, err := getEnvVar("VTB_OUTPUT")
+        if err != nil { return err }
+        profile, err := getEnvVar("VTB_PROFILE")
+        if err != nil { return err }
+
+        profileFlags, ok := gHandbrakeProfile[profile]
+        if !ok {
+            return errors.New(fmt.Sprintf("unknown profile %s", profile))
+        }
+        standardFlags := []string{
+            "-i", input,
+            "-o", output,
+        }
+        cmd := exec.Command(handbrake)
+        cmd.Args = append(cmd.Args, standardFlags...)
+        cmd.Args = append(cmd.Args, profileFlags...)
+        cmd.Stdin = os.Stdin
+        cmd.Stdout = os.Stdout
+        cmd.Stderr = os.Stderr
+        fmt.Println("starting Handbrake....")
+        if err := cmd.Run(); err != nil { return err }
+        fmt.Println("...handbrake finished")
+
         return nil
     }
 
