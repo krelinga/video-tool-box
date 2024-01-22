d89474934ee020568fda8b8232531af7a1f50c7a
Andy Kreling <krelinga@gmail.com>
2024-01-22 05:02:58 +0000

    refactor all of the test container stuff to a new struct type.

--- e2e_test.go
+++ e2e_test.go
@@ -8,26 +8,37 @@ import (
     "github.com/google/uuid"
 )
 
-func buildContainer(t *testing.T, containerId string) {
+type testContainer struct {
+    containerId string
+}
+
+func newTestContainer() testContainer {
+    return testContainer{
+        containerId: fmt.Sprintf("vtb-e2e-test-%s", uuid.NewString()),
+    }
+}
+
+
+func (tc testContainer) Build(t *testing.T) {
     t.Helper()
-    cmd := exec.Command("docker", "image", "build", "-t", containerId, ".")
+    cmd := exec.Command("docker", "image", "build", "-t", tc.containerId, ".")
     if err := cmd.Run(); err != nil {
         t.Fatalf("could not build docker container: %s", err)
     }
     t.Log("Finished building docker container.")
 }
 
-func deleteContainer(t *testing.T, containerId string) {
+func (tc testContainer) Delete(t *testing.T) {
     t.Helper()
-    cmd := exec.Command("docker", "image", "rm", containerId)
+    cmd := exec.Command("docker", "image", "rm", tc.containerId)
     if err := cmd.Run(); err != nil {
         t.Fatalf("could not delete docker container: %s", err)
     }
     t.Log("Finished deleting docker container.")
 }
 
-func runVtb(containerId string, args... string) error {
-    cmd := exec.Command("docker", "run", "--rm", "-t", containerId)
+func (tc testContainer) Run(args... string) error {
+    cmd := exec.Command("docker", "run", "--rm", "-t", tc.containerId)
     cmd.Args = append(cmd.Args, args...)
     return cmd.Run()
 }
@@ -36,12 +47,11 @@ func TestDockerBuildAndRun(t *testing.T) {
     if testing.Short() {
         t.Skip("skipping test in short mode.")
     }
+    tc := newTestContainer()
+    tc.Build(t)
+    defer tc.Delete(t)
 
-    containerId := fmt.Sprintf("vtb-e2e-test-%s", uuid.NewString())
-    buildContainer(t, containerId)
-    defer deleteContainer(t, containerId)
-
-    if err := runVtb(containerId); err != nil {
+    if err := tc.Run(); err != nil {
         t.Errorf("error running vtb: %s", err)
     }
 }
