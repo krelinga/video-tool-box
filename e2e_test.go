ba6c053ca41717bffebaee8c4d5abbf61b690a2b
Andy Kreling <krelinga@gmail.com>
2024-01-22 02:13:01 +0000

    verify that 'go test' command keeps the existing working directory.

--- e2e_test.go
+++ e2e_test.go
@@ -2,10 +2,12 @@ package main
 
 import (
     "testing"
+    "os"
 )
 
 func TestSomething(t *testing.T) {
     if testing.Short() {
         t.Skip("skipping test in short mode.")
     }
+    t.Log(os.Getenv("PWD"))
 }
