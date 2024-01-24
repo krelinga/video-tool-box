a13a7feba4304aa4de7f8dd695a04181025178e4
Andy Kreling <krelinga@gmail.com>
2024-01-24 01:00:41 +0000

    move setting of HOME out of setUpTempDir().

--- state_test.go
+++ state_test.go
@@ -9,10 +9,7 @@ func setUpTempDir(t *testing.T) string {
     t.Helper()
     tempDir, err := os.MkdirTemp("", "state_test.go")
     if err != nil {
-        t.Fatal("could not create fake homedir")
-    }
-    if err := os.Setenv("HOME", tempDir) ; err != nil {
-        t.Fatal("could not set fake homedir")
+        t.Fatal("could not create tempdir")
     }
     return tempDir
 }
@@ -26,6 +23,9 @@ func tearDownTempDir(t *testing.T, tempDir string) {
 
 func TestReadAndWriteStateFile(t *testing.T) {
     tempDir := setUpTempDir(t)
+    if err := os.Setenv("HOME", tempDir) ; err != nil {
+        t.Fatal("could not set fake homedir")
+    }
     defer tearDownTempDir(t, tempDir)
 
     errorToolState := toolState {
