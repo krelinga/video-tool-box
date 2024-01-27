3f8218755eac9bc62cbb7ecefc165254be67741b
Andy Kreling <krelinga@gmail.com>
2024-01-27 06:10:28 +0000

    refactor code to test 'meta' command for unconfigured project into a helper function.

--- rip_test.go
+++ rip_test.go
@@ -118,16 +118,19 @@ func TestRipSequence(t *testing.T) {
         }
         return true
     }
+    testMetaUnconfigured := func() {
+        if !runNoError("meta") {
+            return
+        }
+        initMetaOut := ta.Stdout().String()
+        noProjectConfigured := "no project configured"
+        if !strings.Contains(initMetaOut, noProjectConfigured) {
+            t.Errorf("Unexpected 'meta' output for no configured project: %s", initMetaOut)
+        }
+        ta.Reset()
+    }
 
-    if !runNoError("meta") {
-        return
-    }
-    initMetaOut := ta.Stdout().String()
-    noProjectConfigured := "no project configured"
-    if !strings.Contains(initMetaOut, noProjectConfigured) {
-        t.Errorf("Unexpected 'meta' output for no configured project: %s", initMetaOut)
-    }
-    ta.Reset()
+    testMetaUnconfigured()
 
     // Project names don't have to be quoted on the shell, so we pass
     // "Test" and "Movie" as two separate strings here.
