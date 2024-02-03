ab7217cc0bedbdb73c0ef9e4d4e2922a9a3a4b2a
Andy Kreling <krelinga@gmail.com>
2024-02-03 23:59:35 +0000

    generate grpc protobuffer services.

--- pb/build.sh
+++ pb/build.sh
@@ -3,4 +3,8 @@
 export PATH="${PATH}:$HOME/go/bin"
 
 rm *.pb.go
-protoc -I=. --go_out=. --go_opt=paths=source_relative *.proto
+protoc \
+    -I=. \
+    --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    *.proto
