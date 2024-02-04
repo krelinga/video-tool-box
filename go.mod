9e7a205cca69cdc5e6c52d70aa9a358439315ce3
Andy Kreling <krelinga@gmail.com>
2024-02-04 00:28:00 +0000

    implement hello world method in tcserver.

--- go.mod
+++ go.mod
@@ -6,10 +6,17 @@ require (
 	github.com/dustin/go-humanize v1.0.1
 	github.com/google/uuid v1.5.0
 	github.com/urfave/cli/v2 v2.27.1
+	google.golang.org/grpc v1.61.0
+	google.golang.org/protobuf v1.32.0
 )
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
 )
--- tcserver/main.go
+++ tcserver/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+    "context"
+    "errors"
+    "fmt"
+    "log"
+    "net"
+    "os"
+    "strconv"
+
+    "github.com/krelinga/video-tool-box/pb"
+    "google.golang.org/grpc"
+)
+
+type tcServer struct {
+    pb.UnimplementedTCServerServer
+}
+
+func (tcs *tcServer) HelloWorld(ctx context.Context, req *pb.HelloWorldRequest) (*pb.HelloWorldReply, error) {
+    rep := &pb.HelloWorldReply{
+        Out: req.In,
+    }
+    return rep, nil
+}
+
+func getPort() (int, error) {
+    const envVar = "VTB_TCSERVER_PORT"
+    portString := os.Getenv(envVar)
+    if len(portString) == 0 {
+        return 0, errors.New(fmt.Sprintf("set %s env var", envVar))
+    }
+    port, err := strconv.Atoi(portString)
+    if err != nil {
+        return 0, errors.New(fmt.Sprintf("env var %s should be a port number, saw %s", envVar, portString))
+    }
+    return port, nil
+}
+func mainOrError() error {
+    fmt.Println("hello world!")
+    port, err := getPort()
+    if err != nil {
+        return err
+    }
+    lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+    if err != nil {
+        return err
+    }
+    grpcServer := grpc.NewServer()
+    pb.RegisterTCServerServer(grpcServer, &tcServer{})
+    grpcServer.Serve(lis)  // Runs as long as the server is alive.
+
+    return nil
+}
 
 func main() {
-    fmt.Println("hello world!")
+    if err := mainOrError(); err != nil {
+        log.Fatal(err)
+    }
 }
