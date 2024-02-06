755772f60b331bca6dc4b99affe610c600f3dd04
Andy Kreling <krelinga@gmail.com>
2024-02-06 22:27:13 +0000

    actually do a transcode on the server side instead of just a copy.

--- tcserver/rpc.go
+++ tcserver/rpc.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "context"
+    "errors"
     "fmt"
     "os"
     "os/exec"
@@ -33,6 +34,18 @@ func (tcs *tcServer) HelloWorld(ctx context.Context, req *pb.HelloWorldRequest) 
     return rep, nil
 }
 
+type handbrakeFlags []string
+
+var gHandbrakeProfile = map[string]handbrakeFlags{
+    "mkv_h265_1080p30": {
+        "-Z", "Matroska/H.265 MKV 1080p30",
+        "--all-audio",
+        "--non-anamorphic",
+        "--all-subtitles",
+        "--subtitle-burned=none",
+    },
+}
+
 func (tcs *tcServer) TranscodeOneFile(ctc context.Context, req *pb.TranscodeOneFileRequest) (*pb.TranscodeOneFileReply, error) {
     fmt.Printf("TranscodeOneFile: %v\n", req)
     inPath, err := translatePath(req.InPath)
@@ -44,7 +57,21 @@ func (tcs *tcServer) TranscodeOneFile(ctc context.Context, req *pb.TranscodeOneF
         return nil, err
     }
 
-    // Temporary, until we get handbrake hooked up.
-    copyCmd := exec.Command("cp", inPath, outPath)
-    return &pb.TranscodeOneFileReply{}, copyCmd.Run()
+    const profile = "mkv_h265_1080p30"
+    profileFlags, ok := gHandbrakeProfile[profile]
+    if !ok {
+        return nil, errors.New(fmt.Sprintf("unknown profile %s", profile))
+    }
+    standardFlags := []string{
+        "-i", inPath,
+        "-o", outPath,
+    }
+
+    cmd := exec.Command("/usr/bin/HandBrakeCLI")
+    cmd.Args = append(cmd.Args, standardFlags...)
+    cmd.Args = append(cmd.Args, profileFlags...)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    return &pb.TranscodeOneFileReply{}, cmd.Run()
 }
