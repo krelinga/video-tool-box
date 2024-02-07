475a3821caab98444c43f3508b149cfccd57f80f
Andy Kreling <krelinga@gmail.com>
2024-02-07 01:23:08 +0000

    fmt.Errorf() in env.go

--- env.go
+++ env.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "errors"
     "fmt"
     "os"
 )
@@ -9,8 +8,7 @@ import (
 func getEnvVar(name string) (value string, err error) {
     value = os.Getenv(name)
     if len(value) == 0 {
-        errStr := fmt.Sprintf("could not read %s env var", name)
-        err = errors.New(errStr)
+        err = fmt.Errorf("could not read %s env var", name)
     }
     return
 }
