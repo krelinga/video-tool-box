3d7aa8abfbd2b5eea742de9e90fc5413e84d09cc
Andy Kreling <krelinga@gmail.com>
2024-02-20 22:09:01 +0000

    push version 0 part 1: just push the current path to the nas.  Don't actually push yet, but figure out source and destination paths.

--- push.go
+++ push.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "errors"
+    "fmt"
+    "path/filepath"
 
     cli "github.com/urfave/cli/v2"
 )
@@ -15,5 +17,40 @@ func cmdCfgPush() *cli.Command {
 }
 
 func cmdPush(c *cli.Context) error {
+    tp, ok := toolPathsFromContext(c.Context)
+    if !ok {
+        return errors.New("toolPaths not present in context")
+    }
+    ts, err := readToolState(tp.StatePath())
+    if err != nil {
+        return err
+    }
+
+    cwd := tp.CurrentDir()
+    nasSubDir, err := func() (string, error) {
+        switch ts.Pt {
+        case ptUndef:
+            return "", errors.New("Not in a rip project.")
+        case ptMovie:
+            return "Movies", nil
+        case ptShow:
+            return "Shows", nil
+        default:
+            return "", fmt.Errorf("Unexpected ProjectType value %v", ts.Pt)
+        }
+    }()
+    if err != nil {
+        return err
+    }
+    title := filepath.Base(cwd)
+    outPath := filepath.Join(tp.NasMountDir(), nasSubDir, title)
+
+    fmt.Fprintf(c.App.Writer, "Will copy %s to %s.\nConfirm (y/N)? ", cwd, outPath)
+    var confirm string
+    fmt.Fscanf(c.App.Reader, "%s", &confirm)
+    if confirm != "y" {
+        return nil
+    }
+
     return errors.New("'push' command is not implemented.")
 }
