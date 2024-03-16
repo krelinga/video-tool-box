389923132b56e95663787d22bed58c0d38516365
Andy Kreling <krelinga@gmail.com>
2024-03-16 22:12:49 +0000

    call the mkvinfo command

--- mkvinfoserver/rpc.go
+++ mkvinfoserver/rpc.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+    "bytes"
     "context"
     "fmt"
-    "os"
+    "os/exec"
 
     "github.com/krelinga/video-tool-box/pb"
 )
@@ -14,11 +15,15 @@ type MkvInfoServer struct {
 
 func (mis *MkvInfoServer) GetMkvInfo(ctx context.Context, req *pb.GetMkvInfoRequest) (*pb.GetMkvInfoReply, error) {
     fmt.Printf("Saw request: %v\n", req)
-    if _, err := os.Stat(req.In); err != nil {
-        return nil, fmt.Errorf("Could not stat %s: %w", req.In, err)
+    cmd := exec.Command("mkvinfo", req.In)
+    b := &bytes.Buffer{}
+    cmd.Stdout = b
+    cmd.Stderr = b
+    if err := cmd.Run(); err != nil {
+        return nil, fmt.Errorf("Could not run mkvinfo tool: %w", err)
     }
     reply := &pb.GetMkvInfoReply{
-        Out: "bar",
+        Out: b.String(),
     }
     return reply, nil
 }
