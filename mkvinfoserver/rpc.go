baba91ccbad6b6dac4edfb237a41b8679d447159
Andy Kreling <krelinga@gmail.com>
2024-03-17 03:29:48 +0000

    rudimentary parsing of mkvmerge json output.

--- mkvinfoserver/rpc.go
+++ mkvinfoserver/rpc.go
@@ -5,6 +5,7 @@ import (
     "context"
     "fmt"
     "os/exec"
+    "strings"
 
     "github.com/krelinga/video-tool-box/pb"
 )
@@ -20,10 +21,24 @@ func (mis *MkvInfoServer) GetMkvInfo(ctx context.Context, req *pb.GetMkvInfoRequ
     cmd.Stdout = b
     cmd.Stderr = b
     if err := cmd.Run(); err != nil {
-        return nil, fmt.Errorf("Could not run mkvinfo tool: %w", err)
+        return nil, fmt.Errorf("Could not run mkvmerge tool: %w", err)
+    }
+    mkvmergeBytes := b.Bytes()
+    j, err := newMkvmergeJson(mkvmergeBytes)
+    if err != nil {
+        return nil, fmt.Errorf("Could not parse mkvmerge output: %w", err)
+    }
+    parts := []string{}
+    for _, t := range j.Tracks {
+        if t.Type != "audio" {
+            continue
+        }
+        summary := fmt.Sprintf("Track ID %d: %d channels named %s language %s", t.Id, t.Properties.AudioChannels, t.Properties.TrackName, t.Properties.Language)
+        parts = append(parts, summary)
     }
     reply := &pb.GetMkvInfoReply{
-        Out: b.String(),
+        Out: string(mkvmergeBytes),
+        Summary: strings.Join(parts, "\n"),
     }
     return reply, nil
 }
