aa7791849b6e7cc7cda0b0fbab19a01c57e8cddc
Andy Kreling <krelinga@gmail.com>
2024-04-27 20:10:38 +0000

    base/build.sh checks for multiarch builder and creates it if missing.

--- base/build.sh
+++ base/build.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/bash
 
-/usr/bin/docker buildx build --builder=myBuilder --push \
+if ! /usr/bin/docker buildx ls | egrep -q "^multiarch" ; then
+    /usr/bin/docker buildx create \
+        --name multiarch \
+        --bootstrap
+fi
+
+/usr/bin/docker buildx build --builder=multiarch --push \
     --platform linux/amd64,linux/arm64 \
     --tag krelinga/video-tool-box-base:buildx-latest .
