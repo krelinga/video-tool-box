4d883c228fb439e1df8c86820848f5a09a829bb8
Andy Kreling <krelinga@gmail.com>
2024-04-27 21:33:33 +0000

    update mkvinfoserver_build_and_push.sh to use multiarch builder.

--- mkvinfoserver_build_and_push.sh
+++ mkvinfoserver_build_and_push.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/bash
+#
+if ! /usr/bin/docker buildx ls | egrep -q "^multiarch" ; then
+    /usr/bin/docker buildx create \
+        --name multiarch \
+        --bootstrap
+fi
 
-/usr/bin/docker buildx build --builder=myBuilder --push \
+/usr/bin/docker buildx build --builder=multiarch --push \
     --platform linux/amd64,linux/arm64 \
     -f mkvinfoserver.Dockerfile \
     --tag krelinga/video-tool-box-mkvinfoserver:buildx-latest .
