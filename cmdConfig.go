1f0c2890bf17a9fefef5a1dfbfc3b56e3015f265
Andy Kreling <krelinga@gmail.com>
2024-05-09 15:38:35 +0000

    allow changes to config in  command.

--- cmdConfig.go
+++ cmdConfig.go
@@ -13,6 +13,12 @@ func cmdCfgConfig() *cli.Command {
         Usage: "show or modify config",
         Description: "show or modify config",
         Action: cmdConfig,
+        Flags: []cli.Flag{
+            &cli.StringFlag{
+                Name: "MkvUtilServerTarget",
+                Usage: "set RPC target for Mkv Util Server.",
+            },
+        },
     }
 }
 
@@ -24,6 +30,17 @@ func cmdConfig(c *cli.Context) error {
     cfg, err := readConfig(tp.ConfigPath())
     if err != nil {
         return err
+    }
+
+    needWrite := false
+    if t := c.String("MkvUtilServerTarget"); len(t) > 0 {
+        needWrite = true
+        cfg.MkvUtilServerTarget = t
+    }
+    if needWrite {
+        if err := writeConfig(cfg, tp.ConfigPath()); err != nil {
+            return err
+        }
     }
     _, err = fmt.Fprintf(c.App.Writer, "%s", cfg)
     return err
