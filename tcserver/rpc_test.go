377f361366ba4408506d7c44fd4f32fae729e61e
Andy Kreling <krelinga@gmail.com>
2024-06-11 15:46:38 +0000

    check for error from TCServer in a loop. Verify that we don't have correct behavior right now.

--- tcserver/rpc_test.go
+++ tcserver/rpc_test.go
@@ -4,6 +4,8 @@ import (
     "context"
     "os"
     "path/filepath"
+    "strings"
+    "time"
     "testing"
 
     "github.com/krelinga/video-tool-box/pb"
@@ -36,13 +38,43 @@ func TestOutputPathExists(t *testing.T) {
     tcServer := newTcServer(statePath, defaultProfile)
 
 
+    const name = "test"
     startReq := &pb.StartAsyncTranscodeRequest{
-        Name: "test",
+        Name: name,
         InPath: inPath,
         OutPath: outPath,
     }
     _, err = tcServer.StartAsyncTranscode(context.Background(), startReq)
     if err != nil {
         t.Fatal(err)
+    }
+
+    checkForError := func() (retry bool) {
+        checkReq := &pb.CheckAsyncTranscodeRequest{
+            Name: name,
+        }
+        checkReply, err := tcServer.CheckAsyncTranscode(context.Background(), checkReq)
+        if err != nil {
+            t.Fatal(err)
+        }
+        if checkReply.State == pb.CheckAsyncTranscodeReply_STATE_IN_PROGRESS {
+            retry = true
+            return
+        }
+        failed := checkReply.State == pb.CheckAsyncTranscodeReply_STATE_FAILED
+        correctError := strings.Contains(checkReply.ErrorMessage, "already exists")
+        if failed && correctError {
+            // This is our expected case.
+            retry = false
+            return
+        }
+        // Otherwise we ended up in some unexpected case.
+        t.Error(checkReply)
+        retry = false
+        return
+    }
+    for checkForError() {
+        t.Log("transcode in-progress, retrying...")
+        time.Sleep(time.Millisecond * 100)
     }
 }
