c78ddfb50fd3c8373390763b9496ca801aded2eb
Andy Kreling <krelinga@gmail.com>
2024-06-11 15:24:46 +0000

    write empty input and output files.

--- tcserver/rpc_test.go
+++ tcserver/rpc_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "os"
+    "path/filepath"
     "testing"
 
     //"github.com/krelinga/video-tool-box/pb"
@@ -19,4 +20,13 @@ func TestOutputPathExists(t *testing.T) {
             t.Fatal(err)
         }
     }()
+    touch := func(path string) {
+        if err := os.WriteFile(path, []byte(""), 0777); err != nil {
+            t.Fatal(err)
+        }
+    }
+    inPath := filepath.Join(tmpDir, "in.mkv")
+    outPath := filepath.Join(tmpDir, "out.mkv")
+    touch(inPath)
+    touch(outPath)
 }
