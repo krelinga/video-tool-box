d0fb2dccae29d03097f9afb5ba569031fa768b82
Andy Kreling <krelinga@gmail.com>
2024-06-22 06:30:39 +0000

    helper method to get ints from the environment.

--- tcserver/env.go
+++ tcserver/env.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "os"
+    "strconv"
 )
 
 func getEnvVar(name string) (string, error) {
@@ -11,4 +12,12 @@ func getEnvVar(name string) (string, error) {
         return "", fmt.Errorf("env var %s is not set", name)
     }
     return value, nil
+}
+
+func getEnvVarInt(name string) (int, error) {
+    strVal, err := getEnvVar(name)
+    if err != nil {
+        return 0, err
+    }
+    return strconv.Atoi(strVal)
 }
