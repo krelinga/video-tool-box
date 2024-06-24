aca5fed8641cb67353b088b6375a9ebe61bde424
Andy Kreling <krelinga@gmail.com>
2024-06-24 02:59:17 +0000

    delete hello world server RPC implementation.

--- tcserver/rpc.go
+++ tcserver/rpc.go
@@ -3,7 +3,6 @@ package main
 import (
     "context"
     "fmt"
-    "os"
     "strings"
 
     "github.com/krelinga/video-tool-box/pb"
@@ -22,22 +21,6 @@ func newTcServer(defaultProfile string, tc *transcoder.Transcoder) *tcServer {
         defaultProfile: defaultProfile,
         tc: tc,
     }
-}
-
-func (tcs *tcServer) HelloWorld(ctx context.Context, req *pb.HelloWorldRequest) (*pb.HelloWorldReply, error) {
-    fmt.Printf("Saw request: %v\n", req)
-    fileSize := func() int64 {
-        stat, err := os.Stat(req.In)
-        if err != nil {
-            return -1
-        }
-        return stat.Size()
-    }()
-    rep := &pb.HelloWorldReply{
-        Out: req.In,
-        FileSize: fileSize,
-    }
-    return rep, nil
 }
 
 func (tcs *tcServer) StartAsyncTranscode(ctx context.Context, req *pb.StartAsyncTranscodeRequest) (*pb.StartAsyncTranscodeReply, error) {
