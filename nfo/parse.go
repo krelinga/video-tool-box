41d50fd81fb56b562b51f07ecd7b73f458bee1a5
Andy Kreling <krelinga@gmail.com>
2024-09-02 01:46:09 +0000

    nfo parsing for movies.

---
+++ nfo/parse.go
@@ -0,0 +1,86 @@
+package nfo
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+)
+
+type nfoMovie struct {
+	XMLName  xml.Name     `xml:"movie"`
+	Tags     []string     `xml:"tag"`
+	FileInfo *nfoFileInfo `xml:"fileinfo"`
+}
+
+type nfoFileInfo struct {
+	StreamDetails *nfoStreamDetails `xml:"streamdetails"`
+}
+
+type nfoStreamDetails struct {
+	Video *nfoVideo `xml:"video"`
+}
+
+type nfoVideo struct {
+	Width  int `xml:"width"`
+	Height int `xml:"height"`
+}
+
+func readFromFile(filename string) (*nfoMovie, error) {
+	// Open the XML file
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a new XML decoder
+	decoder := xml.NewDecoder(file)
+
+	// Parse the XML content
+	entry := &nfoMovie{}
+	err = decoder.Decode(entry)
+	if err != nil {
+		if err == io.EOF {
+			return nil, fmt.Errorf("no movie data found")
+		}
+		return nil, err
+	}
+
+	return entry, nil
+}
+
+type Content struct {
+	Tags   []string
+	Width  int
+	Height int
+}
+
+func Parse(filename string) (*Content, error) {
+	movie, err := readFromFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if movie.FileInfo == nil {
+		return nil, fmt.Errorf("no file info found")
+	}
+	fileInfo := movie.FileInfo
+	if fileInfo.StreamDetails == nil {
+		return nil, fmt.Errorf("no stream details found")
+	}
+	streamDetails := fileInfo.StreamDetails
+	if streamDetails.Video == nil {
+		return nil, fmt.Errorf("no video stream details found")
+	}
+	video := streamDetails.Video
+	if video.Width == 0 || video.Height == 0 {
+		return nil, fmt.Errorf("invalid video resolution")
+	}
+
+	return &Content{
+		Tags:   movie.Tags,
+		Width:  video.Width,
+		Height: video.Height,
+	}, nil
+}
---
+++ nfo/parse_test.go
@@ -0,0 +1,116 @@
+package nfo
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		filename string
+		expected *Content
+		errMsg   string
+	}{
+		{
+			filename: "../testdata/nfo/movies/Beavis and Butt-Head Do America (1996).nfo",
+			expected: &Content{
+				Tags:   []string{"hotel", "sperm", "washington dc, usa", "casino", "sun", "road trip", "las vegas", "adult animation", "based on tv series"},
+				Width:  720,
+				Height: 480,
+			},
+			errMsg: "",
+		},
+		{
+			filename: "../testdata/nfo/movies/The Void (2016).nfo",
+			expected: &Content{
+				Tags:   []string{"nurse", "mutation", "mutant", "morgue", "murder", "hospital", "another dimension", "doctor", "pregnant woman", "cosmic horror", "ax"},
+				Width:  1920,
+				Height: 1080,
+			},
+			errMsg: "",
+		},
+		{
+			filename: "../testdata/nfo/movies/They Live (1988).nfo",
+			expected: &Content{
+				Tags:   []string{"dystopia", "villainess", "alien", "social commentary", "conspiracy", "los angeles, california", "alien invasion", "sunglasses", "glasses", "brawl", "subliminal message", "horror"},
+				Width:  720,
+				Height: 480,
+			},
+			errMsg: "",
+		},
+		{
+			filename: "../testdata/nfo/movies/errors/no_movie.nfo",
+			expected: nil,
+			errMsg:   "no movie data found",
+		},
+		{
+			filename: "../testdata/nfo/movies/errors/no_fileinfo.nfo",
+			expected: nil,
+			errMsg:   "no file info found",
+		},
+		{
+			filename: "../testdata/nfo/movies/errors/no_streamdetails.nfo",
+			expected: nil,
+			errMsg:   "no stream details found",
+		},
+		{
+			filename: "../testdata/nfo/movies/errors/no_video.nfo",
+			expected: nil,
+			errMsg:   "no video stream details found",
+		},
+		{
+			filename: "../testdata/nfo/movies/errors/no_height.nfo",
+			expected: nil,
+			errMsg:   "invalid video resolution",
+		},
+		{
+			filename: "../testdata/nfo/movies/errors/no_width.nfo",
+			expected: nil,
+			errMsg:   "invalid video resolution",
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.filename, func(t *testing.T) {
+			t.Parallel()
+			content, err := Parse(test.filename)
+			if err != nil {
+				if test.errMsg == "" {
+					t.Errorf("Unexpected error: %v", err)
+				} else if err.Error() != test.errMsg {
+					t.Errorf("Expected error message: %q, got: %q", test.errMsg, err.Error())
+				}
+				return
+			}
+
+			if test.errMsg != "" {
+				t.Errorf("Expected error message: %q, got: nil", test.errMsg)
+				return
+			}
+
+			if !equalContent(content, test.expected) {
+				t.Errorf("Expected content: %v, got: %v", test.expected, content)
+			}
+		})
+	}
+}
+
+func equalContent(c1, c2 *Content) bool {
+	if c1 == nil && c2 == nil {
+		return true
+	}
+	if c1 == nil || c2 == nil {
+		return false
+	}
+	if len(c1.Tags) != len(c2.Tags) {
+		return false
+	}
+	for i := range c1.Tags {
+		if c1.Tags[i] != c2.Tags[i] {
+			return false
+		}
+	}
+	return c1.Width == c2.Width && c1.Height == c2.Height
+}
